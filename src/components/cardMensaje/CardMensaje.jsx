4da96ae4883f4e6cd492bec0c879780d543c4ae7
Karisha:) <krshmelendez@gmail.com>
2025-06-16 12:38:20 +0200

    crear componente cardMensaje y paginaDiario

---
+++ src/components/cardMensaje/CardMensaje.jsx
@@ -0,0 +1,14 @@
+import './CardMensaje.css'
+//import { useState } from 'react';
+
+
+function CardMensaje(){
+    return(
+        <div className='card-container'>
+            <p> Yo creo en la supremacía de los gatos</p>
+        </div>
+        
+    );
+}
+
+export default CardMensaje
---
+++ src/pages/diarioPage/DiarioPage.jsx
@@ -0,0 +1,13 @@
+import CardMensaje from "../../components/cardMensaje/CardMensaje";
+import './DiarioPage.css'
+
+function DiarioPage(){
+    return(
+
+        <>
+        <CardMensaje/>
+        </>
+    );
+}
+
+export default DiarioPage
