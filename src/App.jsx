32985170e156494fb91898aae3d4008cd9f797a9
Marta Ibarra <martaa.ibarra@hotmail.com>
2025-06-17 10:07:10 +0200

    Añadida ruta

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,15 @@
-import { Route, Routes } from 'react-router-dom'
-import ReverbPage from './pages/ReverbPage/ReverbPage'
+import { Route, Routes } from "react-router-dom";
+import ReverbPage from "./pages/ReverbPage/ReverbPage";
+import ReverbCard from "./components/ReverbCard/ReverbCard";
 
 function App() {
-
   return (
     <>
-     <Routes>
-
-      <Route path='/reverberacions' element={<ReverbPage />} />  
-
-
-     </Routes>
+      <Routes>
+        <Route path="/mensaje-original/:id" element={<ReverbPage />} />
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
